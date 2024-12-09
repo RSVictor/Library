78a33c8833de6724b62a169a08c9a2668802d7a3
RSVictor <victorribeiro304@hotmail.com>
2024-12-09 20:24:43 -0300

    update User.js

--- library-api/models/user.js
+++ library-api/models/user.js
@@ -1,16 +1,14 @@
-const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 
 // Define o esquema de usuários
-
-const userSchema = new mongoose.Schema({     
-     username: { type: String, required: true }, 
-     email: { type: String, required: true, unique: true }, 
-     password: { type: String, required: true }, 
-     status: { type: String, default: 'Ativo' }, 
-     permissions: { type: String, default: 'Usuario' }
-    
-    });
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    status: { type: String, default: 'Ativo' },
+    permissions: { type: String, default: 'Usuario' }
+});
 
 // Exporta o modelo de usuário
-module.exports = mongoose.models.User || mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema);
+module.exports = User;
