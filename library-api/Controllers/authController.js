4453c014da70fadf2edbb4cb780cfe4d3ac3b763
RSVictor <victorribeiro304@hotmail.com>
2024-12-09 21:32:56 -0300

    atualizado

--- library-api/Controllers/authController.js
+++ library-api/Controllers/authController.js
@@ -1,4 +1,4 @@
-const User = require('../Models/User');
+const User = require(path.join(__dirname, '../Models/User'));
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const express = require('express');
