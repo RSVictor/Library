5d004a60411257f2866e9752ea7bab85c620a2e3
RSVictor <victorribeiro304@hotmail.com>
2024-12-11 17:48:46 -0300

    atualizado

--- library-api/server.js
+++ library-api/server.js
@@ -1,52 +1,12 @@
-require('dotenv').config(); // Carrega variáveis de ambiente do arquivo .env
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const multer = require('multer'); // Importando Multer
-const path = require('path');
-
-// Configuração do Multer para salvar imagens
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'uploads/'); // Pasta onde as imagens serão salvas
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname)); // Gera um nome único para o arquivo
-    },
-});
-const upload = multer({ storage: storage }); // Corrigido para não usar .single aqui
-
 const app = express();
-app.use(cors({
-    origin: 'https://library-8hbh.onrender.com',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
-
-app.use(express.json());
-app.use('/uploads', express.static('uploads')); // Permitir acesso às imagens salvas
-
-// Conexão ao MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB conectado'))
-.catch(err => console.log('Erro ao conectar ao MongoDB:', err));
-
-// Importação das rotas
-const booksRoutes = require('./routes/books')(upload); // Passando Multer para as rotas
-app.use('/api/books', booksRoutes);
-
-const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes);
-
-const userRoutes = require('./routes/userRoutes');
-app.use('/admin/api/users', userRoutes); // Rotas CRUD de usuários
-
 
 const PORT = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('Servidor rodando!');
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
-})
+});
